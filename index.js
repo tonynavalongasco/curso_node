08927633b368190e3c40ca33f20ed20e126d41a6
Antonio <tonynavalongasco@gmail.com>
2022-03-13 20:58:27 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 5000
 
+app.get('/',(req,res)=>{
+res.send('Visitaste la pagina de inicio')
+})
+
+app.listen(port,()=>{
+console.log('En el puerto 5000')
+})
