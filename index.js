e50526dc0c0b56f46bc287fd572e7f4a020b9d79
Antonio <tonynavalongasco@gmail.com>
2022-03-13 23:36:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT
 
+app.use(express.static('public'))
+
 app.get('/',(req,res)=>{
 res.send('Visitaste la pagina de inicio')
 })
