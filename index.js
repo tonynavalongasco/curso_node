6c198386f948d950441d21b2c46977c1a43f262c
Antonio <tonynavalongasco@gmail.com>
2022-03-15 10:41:43 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const port = process.env.PORT
 
 app.use(express.static('public'))
 
-app.get('/',(req,res)=>{
-res.send('Visitaste la pagina de inicio')
+app.get('/hola',(req,res)=>{
+res.send('Como estas?')
 })
 
 app.listen(port,()=>{
